55af745bf5b0652aa991f539f7590113998652ab
Никита Пашков <nikitapashkov@MacBook-Air-Nikita.local>
2022-11-16 06:24:00 +0300

    Add main page functional tests

--- blog/functional_tests.py
+++ blog/functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "The install worked successfully! Congratulations!" in browser.title
+class MainPageTest(unittest.TestCase):
+    """Проверка отображения элементов главной страницы"""
+
+    def setUp(self):
+        """Открытие страницы блога в браузере"""
+
+        self.browser = webdriver.Firefox()
+        self.browser.get("http://localhost:8000")
+
+
+    def tearDown(self):
+        """Закрытие окна браузера"""
+
+        self.browser.close()
+
+
+    def test_title_display(self):
+        """Проверка отображения заголовка сайта во вкладке браузера"""
+        
+        self.assertEqual("ghjslf", self.browser.title)
+
+
+if __name__ == "__main__":
+    unittest.main()
