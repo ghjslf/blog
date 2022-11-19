398baa0cd0752e1b25824a308c6b238409924f60
Никита Пашков <nikitapashkov@MacBook-Air-Nikita.local>
2022-11-20 01:53:08 +0300

    Add main page tests (correct html, title)

--- blog/articles/tests.py
+++ blog/articles/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve 
+from django.test import TestCase 
+from django.http import HttpRequest
 
 from articles.views import main_page
 
@@ -9,3 +10,17 @@ class MainPageTest(TestCase):
 
     def test_correct_url_resolves_to_main_page_view(self):
         self.assertEqual(resolve('/').func, main_page)
+
+    def test_main_page_view_content_contains_correct_html(self):
+        """Проверяем, что view главной страницы возвращает текст обернутый в <html></html>"""
+        request = HttpRequest()
+        response = main_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.endswith('</html>'))
+
+    def test_main_page_view_content_contains_correct_title(self):
+        request = HttpRequest()
+        response = main_page(request)
+        html = response.content.decode('utf-8')
+        self.assertIn('<title>ghjslf</title>', html)
