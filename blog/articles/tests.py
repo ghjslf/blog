55ae8a19faecc31091b98dba98849ba85f73530a
Никита Пашков <nikitapashkov@MacBook-Air-Nikita.local>
2022-11-20 01:16:54 +0300

    Add first unit test

--- blog/articles/tests.py
+++ blog/articles/tests.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from articles.views import home_page
+
+
+class HomePageTest(TestCase):
+    """Проверка домашней страницы"""
+
+    def test_correct_url_resolves_to_home_page_view(self):
+        self.assertEqual(resolve('/').func, home_page)
