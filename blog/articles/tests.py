5de1b7296e46c6fba67ef044ac5bd8f48c709e77
Никита Пашков <nikitapashkov@MacBook-Air-Nikita.local>
2022-11-28 18:05:50 +0300

    Add article page test

--- blog/articles/tests.py
+++ blog/articles/tests.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from articles.views import main_page
 from articles.models import Article, Tag
 
+
 class MainPageTest(TestCase):
     """Проверка главной страницы"""
 
@@ -26,7 +27,7 @@ class MainPageTest(TestCase):
         self.assertIn('<title>Блог Никиты Пашкова</title>', html)
 
     def test_articles_list_display(self):
-
+        """Проверяем, что на главной странице отображаются статьи из базы данных"""
         test_tag = Tag(
             title="test"
         )
@@ -48,25 +49,54 @@ class MainPageTest(TestCase):
         another_test_article.save()
         another_test_article.tags.add(test_tag)
 
-        print(test_article)
-        print(test_article.tags.all())
-
-        print(another_test_article)
-        print(another_test_article.tags.all())
-
         request = HttpRequest()
         response = main_page(request)
         html = response.content.decode('utf-8')
 
-        self.assertIn(test_article.title, html)
-        for tag in test_article.tags.all():
-            self.assertIn(tag.title, html)
-        self.assertNotIn(test_article.content, html)
+        self.assertIn("Title 1", html)
+        self.assertIn("/1", html)
+        self.assertIn("test", html)
+        self.assertNotIn("Content 1", html)
         
-        self.assertIn(another_test_article.title, html)
-        for tag in another_test_article.tags.all():
-            self.assertIn(tag.title, html)
-        self.assertNotIn(another_test_article.content, html)
+        self.assertIn("Title 2", html)
+        self.assertIn("/2", html)
+        self.assertIn("test", html)
+        self.assertNotIn("Content 2", html)
+
+        # self.assertIn(test_article.title, html)
+        # for tag in test_article.tags.all():
+        #     self.assertIn(tag.title, html)
+        # self.assertNotIn(test_article.content, html)
+        
+        # self.assertIn(another_test_article.title, html)
+        # for tag in another_test_article.tags.all():
+        #     self.assertIn(tag.title, html)
+        # self.assertNotIn(another_test_article.content, html)
         
         
-        
+class ArticlePageTest(TestCase):
+    """Проверка страницы конкретной статьи"""
+
+    def test_article_display(self):
+        """Проверяем, что на странице статьи отображаются все ее нужные поля"""
+        test_tag = Tag(
+            title="test"
+        )
+        test_tag.save()
+
+        test_article = Article(
+            title="Title 1",
+            annotation="Annotation 1",
+            content="Content 1"
+        )
+        test_article.save()
+        test_article.tags.add(test_tag)
+
+        request = HttpRequest()
+        response = article_page(request)
+        html = response.content.decode('utf-8')
+
+        self.assertIn("Title 1", html)
+        self.assertNotIn("Annotation 1", html)
+        self.assertIn("test", html)
+        self.assertIn("Content 1", html)
